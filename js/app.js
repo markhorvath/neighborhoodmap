64cd21288b9e9a182a79da63797e04b9dfaf4377
markhorvath <markhorvath7@gmail.com>
2017-02-07 13:23:13 -0500

    added infowindows, list view

---
+++ js/app.js
@@ -0,0 +1,133 @@
+var map;
+var markers = [];
+
+var viewModel;
+
+var model = [{
+    title: 'Imperial Palace',
+    location: {
+      lat: 35.685360,
+      lng: 139.753372
+    }
+  },
+  {
+    title: 'Akihabara',
+    location: {
+      lat: 35.702190,
+      lng: 139.774459
+    }
+  },
+  {
+    title: 'Harajuku',
+    location: {
+      lat: 35.672010,
+      lng: 139.710212
+    }
+  },
+  {
+    title: 'Tokyo Tower',
+    location: {
+      lat: 35.658772,
+      lng: 139.745454
+    }
+  },
+  {
+    title: 'Sensōji',
+    location: {
+      lat: 35.714948,
+      lng: 139.796655
+    }
+  },
+  {
+    title: 'Craft Beer Market',
+    location: {
+      lat: 35.693382,
+      lng: 139.767303
+    }
+  }
+];
+
+function initMap() {
+    //initialize new google map
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 35.652832, lng: 139.839478},
+        zoom: 11,
+        //styles: styles,
+        mapTypeControl: false
+    });
+    //variable to be used in populating markers' infowindows
+    var largeInfowindow = new google.maps.InfoWindow();
+    //For loop to create markers based on Model locations data
+    for (i = 0; i < model.length; i++){
+        var position = model[i].location;
+        var title = model[i].title;
+
+        var marker = new google.maps.Marker({
+            position: position,
+            map: map,
+            title: title,
+            animation: google.maps.Animation.DROP,
+            id: i
+        });
+
+        markers.push(marker);
+
+
+        marker.addListener('click', function() {
+          makeInfoWindow(this, largeInfowindow);
+        });
+    };
+    //function to populate marker-specific infowindow on click
+    function makeInfoWindow(marker, infowindow) {
+        if (infowindow.marker != marker) {
+          infowindow.setContent('');
+          infowindow.marker = marker;
+
+          infowindow.addListener('closeclick', function() {
+            infowindow.marker = null;
+          });
+          var streetViewService = new google.maps.StreetViewService();
+          var radius = 50;
+
+          function getStreetView(data, status) {
+            if (status == google.maps.StreetViewStatus.OK) {
+              var nearStreetViewLocation = data.location.latLng;
+              var heading = google.maps.geometry.spherical.computeHeading(
+                nearStreetViewLocation, marker.position);
+                infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
+                var panoramaOptions = {
+                  position: nearStreetViewLocation,
+                  pov: {
+                    heading: heading,
+                    pitch: 30
+                  }
+                };
+              var panorama = new google.maps.StreetViewPanorama(
+                document.getElementById('pano'), panoramaOptions);
+            } else {
+              infowindow.setContent('<div>' + marker.title + '</div>' +
+                '<div>No Street View Found</div>');
+            }
+          }
+          streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+
+          infowindow.open(map, marker);
+        }
+    }
+};
+
+var Location = function(data) {
+  this.title = ko.observable(data.title);
+};
+
+var ViewModel = function() {
+    var self = this;
+    this.locationList = ko.observableArray([]);
+    model.forEach(function(item){
+      self.locationList.push(new Location(item));
+    });
+};
+
+viewModel = new ViewModel();
+
+ko.applyBindings(viewModel);
